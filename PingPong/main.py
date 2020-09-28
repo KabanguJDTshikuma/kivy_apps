3d628a58a31e0910ef07c16aa251b1f7cae0cc1d
kabangu_cti <kabangufuture@gmail.com>
2020-09-28 14:22:42 +0200

    added a pong.kv file

--- PingPong/main.py
+++ PingPong/main.py
@@ -1,11 +1,30 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
 
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # latest position of the bool = Current velocity + current position
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+# update - moving the ball by calling the move() and other stuff
 class PongGame(Widget):
-    pass
+    def update(self, dt):
+        pass
+
 
 class PongApp(App):
     def build(self):
+        game = PongGame()
+        Clock.aschedule_interval(game.update, 1.0/60.0)
         return PongGame()
 
 
